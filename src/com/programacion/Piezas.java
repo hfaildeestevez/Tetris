d99f14abf05cf2c733da71c0f040b19f6688b5f6
hfaildeestevez <hfaildeestevez@danielcastelao.org>
2021-03-17 12:29:13 +0100

    Metodo rotar

--- src/com/programacion/Piezas.java
+++ src/com/programacion/Piezas.java
@@ -2,4 +2,24 @@ package com.programacion;
 
 public class Piezas {
 
+
+    //Declaramos matriz
+    int [][] coordenada;
+    Tile tl;
+
+
+        public void rotar()
+        {
+            // Rotamos a peza
+            for(var coordenada: coordenada) {
+                coordenada [0]=-coordenada[1];
+                coordenada [1]=coordenada[0]
+            }
+
+
+        }
+
+
+    }
+
 }
