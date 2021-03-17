e60fbb3c1159fc5c552b7e4f5dbf915ab85b13d6
Pablo Pereira Moure <ppereiramoure@danielcastelao.com>
2021-03-17 15:09:10 +0100

    Colocar pieza y colisionar pieza

--- src/com/programacion/Tablero.java
+++ src/com/programacion/Tablero.java
@@ -1,10 +1,24 @@
 package com.programacion;
 
 public class Tablero {
-    int [][] campo;
-    Pieza Pieza;
+
+    Tile [][] campo;
+    Pieza pieza;
     int puntuacion;
     int level;
     int gravedad;
+
+    private void colocarPieza (){
+        for (var coordenada: pieza.coordenadas){
+            int x=coordenada[0];
+            int y=coordenada[1];
+            campo [x][y]=pieza.tile;
+        }
+    }
+
+    private boolean colisionar (int x,int y){
+        return campo[x][y]!=null;
+    }
+
 
 }
