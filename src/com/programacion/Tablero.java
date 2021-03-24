1ee54fc2f256d233f735c7cf42070ebd82b06e86
hfaildeestevez <hfaildeestevez@danielcastelao.org>
2021-03-24 01:24:00 +0100

    para pull

--- src/com/programacion/Tablero.java
+++ src/com/programacion/Tablero.java
@@ -3,14 +3,8 @@ package com.programacion;
 import java.util.Random;
 
 public class Tablero {
-    /**
-     * Declaramos o tableiro cos seguintes parametros
-     * @param puntuacion
-     * @param level
-     * @param delay
-     * ademais damoslle a funcion random
-     */
-    Tile [][] campo;
+
+    Tile[][] campo;
     Pieza pieza;
     int puntuacion;
     int level;
@@ -18,7 +12,7 @@ public class Tablero {
     Random generadorAleatorio;
 
     public Tablero() {
-        generadorAleatorio =new Random();
+        generadorAleatorio = new Random();
         level = 0;
         puntuacion = 0;
         campo = new Tile[10][20];
@@ -28,12 +22,8 @@ public class Tablero {
         } catch (Exception e) {
             System.out.println("A vuelto a haber un error");
         }
-
     }
-    /**
-     * Chamamos ao metodo generarNuevaPieza e añadimoslle unha excepcion para que o limite sea un 6
-       xa que as nosas pezas van enumeradas do 0 ao 6
-     */
+
     private void generarNuevaPieza() {
         try {
             pieza = PiezaFactory.getPieza(generadorAleatorio.nextInt(6));
@@ -41,91 +31,82 @@ public class Tablero {
             System.out.println("Un error sin mas");
         }
         pieza.moverDerecha();
-
     }
-    /**
-     * Chamamos ao metodo colocarPieza e facemos que un foreach recorra o array
-     */
-    private void colocarPieza (){
-        for (var coordenada: pieza.coordenada){
-            int x=coordenada[0];
-            int y=coordenada[1];
-            campo [x][y]=pieza.tile;
+
+    private void colocarPieza() {
+        for (var coordenada : pieza.coordenada) {
+            int x = coordenada[0];
+            int y = coordenada[1];
+            campo[x][y] = pieza.tile;
         }
         generarNuevaPieza();
     }
-    /**
-     * Chamamos ao metodo colisionar e aañdimoslle unha excepcion
-     */
-    private boolean colisionar (int x,int y){
-        try{
-            return x> campo.length-1 || y>campo[0].length-1  || campo[x][y] !=null;
-        }catch(IndexOutOfBoundsException e){
+
+    private boolean colisionar(int x, int y) {
+        try {
+            return x > campo.length - 1 || y > campo[0].length - 1 || campo[x][y] != null;
+        } catch (IndexOutOfBoundsException e) {
             return true;
         }
     }
 
-    private void comprobarLinea (){
-        for (int i =0;i<campo.length;i++){
-            boolean completado=true;
-            for (int j =0; j<campo[0].length;j++){
-                if (campo[i][j]==null){
-                    completado=false;
+    private void comprobarLinea() {
+        boolean completa;
+        for (int j = 0; j < campo[0].length; j++) {//numero de fila (segunda coordenada)
+            completa = true;
+            for (int i = 0; i < campo.length; i++) {
+                if (campo[i][j] == null) {
+                    completa = false;
+                    break;
                 }
             }
-            if (completado){
-                moverPosiciones(i);
+            if (completa) {
+                moverPosiciones(j);
             }
         }
     }
 
-    private void moverPosiciones(int columna){
-        for (int i =0;i<columna;i++){
-            campo [i+1] = campo[i];
+    private void moverPosiciones(int fila) {
+        Tile[][] nuevoCampo = transposeArray(campo);
+        for (int i =fila-1; i >0; i--) {
+            nuevoCampo[i+1]=nuevoCampo[i];
+            System.out.println(String.format("Fila %d movida a %d", i+1,i));
+        }
+        campo = transposeArray(nuevoCampo);
+    }
+
+    public void update() {
+        if (!colisionarPieza(0, 1)) {
+            pieza.moverAbajo();
+        } else {
+            colocarPieza();
+            comprobarLinea();
         }
     }
 
-    public void update (){
-        if (!colisionarPieza(0,1)){
-            pieza.moverAbajo();
-        }
-        else{
-            colocarPieza();
-            comprobarLinea();
-            //generarNuevaPieza();
-        }
-    }
-    /**
-     * Chamamos ao metodo colisionarPieza
-     */
     private boolean colisionarPieza(int desplazamientox, int desplazamientoy) {
         boolean colisiona = false;
-        for (var coordenada:pieza.coordenada) {
-            if (colisionar(coordenada[0]+desplazamientox,coordenada[1]+desplazamientoy))
-                colisiona=true;
+        for (var coordenada : pieza.coordenada) {
+            if (colisionar(coordenada[0] + desplazamientox, coordenada[1] + desplazamientoy)) {
+                colisiona = true;
+            }
         }
         return colisiona;
     }
-    /**
-     * Chamamos ao metodo moverPiezaDerecha
-     */
-    public void moverPiezaDerecha(){
-        if (!colisionarPieza(1,0)){
+
+    public void moverPiezaDerecha() {
+        if (!colisionarPieza(1, 0)) {
             pieza.moverDerecha();
         }
     }
-    /**
-     * Chamamos ao metodo moverPiezaIzquierda
-     */
-    public void moverPiezaIzquierda(){
-        if (!colisionarPieza(-1,0)){
+
+    public void moverPiezaIzquierda() {
+        if (!colisionarPieza(-1, 0)) {
             pieza.moverIzquierda();
         }
     }
-    /**
-     * Chamamos ao metodo RotarPieza
-     */
-    public void rotarPieza(){
+
+    public void rotarPieza() {
         pieza.rotar();
         if (colisionarPieza(0, 0)) {
             pieza.rotar();
@@ -133,13 +114,40 @@ public class Tablero {
             pieza.rotar();
         }
     }
-   /**
- * Chamamos ao metodo moverabajo
- */
-    public void moverPiezaAbajo(){
-        if (!colisionarPieza(0,1)){
+
+    public void moverPiezaAbajo() {
+        if (!colisionarPieza(0, 1)) {
             pieza.moverAbajo();
         }
     }
-}
 
+    public static Tile[][] transposeArray(Tile[][] array) {
+        int m = array.length;
+        int n = array[0].length;
+
+        Tile[][] transposedArray = new Tile[n][m];
+
+        for (int x = 0; x < n; x++) {
+            for (int y = 0; y < m; y++) {
+                transposedArray[x][y] = array[y][x];
+            }
+        }
+
+        return transposedArray;
+    }
+
+    public  String printCampo() {
+        int m = campo.length;
+        int n = campo[0].length;
+
+        String tmp = "";
+        for (int y = 0; y < m; y++) {
+            for (int x = 0; x < n; x++) {
+                tmp = tmp + campo[y][x] + " ";
+            }
+            tmp = tmp + "\n";
+        }
+
+        return tmp;
+    }
+}
