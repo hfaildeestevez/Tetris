137c25b7c538f28d67751492254b2b64a0e73943
hfaildeestevez <hfaildeestevez@danielcastelao.org>
2021-05-12 17:26:39 +0200

    JAVADOC

--- src/com/Logica/Tetris.java
+++ src/com/Logica/Tetris.java
@@ -2,13 +2,20 @@ package com.Logica;
 
 
 import Interfaz.Ventana;
+import Piezas.LectorPuntucion;
 
 public class Tetris {
+
+    /**
+     *
+     * object Ventana para llamar a la interfaz grafica
+     *Importamos la clase LectorPuntuacion para guardar las puntuaciones de los jugadores
+     */
 
     public static void main(String[] args) {
         Ventana ventana= new Ventana();
         ventana.setVisible(true);
-        com.programacion.LectorPuntucion.leer();
+        LectorPuntucion.leer();
     }
 }
 
