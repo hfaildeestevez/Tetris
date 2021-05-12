68d0b52425b0a0f4dd81f63db75af17e62ea3013
hfaildeestevez <hfaildeestevez@danielcastelao.org>
2021-05-12 17:28:55 +0200

    .

--- src/com/Logica/Tetris.java
+++ src/com/Logica/Tetris.java
@@ -11,7 +11,7 @@ public class Tetris {
      * object Ventana para llamar a la interfaz grafica
      *Importamos la clase LectorPuntuacion para guardar las puntuaciones de los jugadores
      */
-
+//comentario para subir
     public static void main(String[] args) {
         Ventana ventana= new Ventana();
         ventana.setVisible(true);
